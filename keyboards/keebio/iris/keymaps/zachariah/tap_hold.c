992f8ef6e0a467dec87f1abc1e375c760a3d6b0a
Zachariah <wild.zachariah@gmail.com>
2023-06-26 11:21:18 -0500

    Fix variable name

--- keyboards/keebio/iris/keymaps/zachariah/tap_hold.c
+++ keyboards/keebio/iris/keymaps/zachariah/tap_hold.c
@@ -1,9 +1,7 @@
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LALT_T(KC_BSPACE):
+        case LALT_T(KC_BSPC):
             return false;
-        // case LGUI_T(KC_BSPACE):
-        //     return false;
         default:
             return true;
     }
